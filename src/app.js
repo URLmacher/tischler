085296b644025fb5cb0ce262e73ec8f623abeae9
Patrick <urlmacher@gmail.com>
2019-10-30 08:21:19 +0100

    basic nav

--- src/app.js
+++ src/app.js
@@ -1,3 +1,24 @@
+import { PLATFORM } from 'aurelia-framework';
+
 export class App {
-  message = 'Hello World!';
+  configureRouter(config, router) {
+    this.router = router;
+    config.title = 'Tischler';
+    config.map([
+      {
+        route: ['', 'home'],
+        name: 'home',
+        moduleId: PLATFORM.moduleName('pages/home'),
+        nav: true,
+        title: 'Home'
+      },
+      {
+        route: 'products',
+        name: 'products',
+        moduleId: PLATFORM.moduleName('pages/products'),
+        nav: true,
+        title: 'Leistungen'
+      }
+    ]);
+  }
 }
