f7471ebd2db53f77076ebaafbe9ba4b92d0359fc
Patrick <urlmacher@gmail.com>
2019-11-03 19:42:23 +0100

    nav open and close

--- src/resources/elements/navigation.js
+++ src/resources/elements/navigation.js
@@ -8,7 +8,19 @@ export class Navigation {
     this.subLinks = ['Impressum', 'Anfahrt', 'Datenschutz', 'AGB'];
   }
 
+  attached() {
+    document.body.addEventListener('click', e => {
+      if (!this.navbar.contains(e.target) && e.target !== this.openNavBtn) this.navOpen = false;
+    });
+  }
+
   openNav() {
     this.navOpen = !this.navOpen;
+  }
+
+  closeNav(e) {
+    if (e.target === e.currentTarget) {
+      this.navOpen = false;
+    }
   }
 }
