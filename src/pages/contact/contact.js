9b2403ca63ea7476c379c515e1f7c8681197c490
Patrick <urlmacher@gmail.com>
2019-11-11 15:10:45 +0100

    form base

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -1,8 +1,32 @@
+import { HttpClient } from 'aurelia-fetch-client';
+
 export class Contact {
   constructor() {
     this.inputLabel1 = 'Name';
     this.inputLabel2 = 'Email';
     this.textAreaLabel = 'Nachricht';
     this.buttonText = 'Senden';
+  }
+
+  attached() {
+    this.serverCall();
+  }
+
+  serverCall() {
+    let postData = { test: 'test' };
+    let httpClient = new HttpClient();
+
+    httpClient
+      .fetch('http://tischler.loc/backend/mail.php', {
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        method: 'post',
+        body: JSON.stringify(postData)
+      })
+      .then(result => result.json())
+      .then(data => {
+        console.log(data);
+      });
   }
 }
