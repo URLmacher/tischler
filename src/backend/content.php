ba57124bafb94cad47d9acec8fab195c1253fa87
Patrick <urlmacher@gmail.com>
2019-12-05 17:11:47 +0100

    added backend security

--- src/backend/content.php
+++ src/backend/content.php
@@ -4,19 +4,33 @@ $answer = new stdClass();
 
 // Todo : only allow content tables
 if (!empty($_GET['area'])) {
-    $area = filter_var(trim($_GET['area']), FILTER_SANITIZE_STRING);
-    $answer->content = getContent($area);
+  $area = filter_var(trim($_GET['area']), FILTER_SANITIZE_STRING);
+
+  switch ($area) {
+    case 'about':
+      $answer->content = getContent($area);
+      break;
+    case 'impressum':
+      $answer->content = getContent($area);
+      break;
+    case 'datenschutz':
+      $answer->content = getContent($area);
+      break;
+    case 'products':
+      $answer->content = getContent($area);
+      break;
+  }
 }
 
 function getContent($area)
 {
   include 'dbconnect.php';
 
-    $sql = 'SELECT * FROM ' . $area;
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute();
-    $content = $stmt->fetchAll();
-    return $content;
+  $sql = 'SELECT * FROM ' . $area;
+  $stmt = $pdo->prepare($sql);
+  $stmt->execute();
+  $content = $stmt->fetchAll();
+  return $content;
 }
 
 
